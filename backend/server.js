70bc69b24c1c3b87a77b9bf37429965eeab6426a
marlar-tz <marlarhal55@gmail.com>
2025-10-20 10:14:16 +0700

    MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -9,17 +10,27 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use((req,res,next)=>{
-    console.log(req.path,req.method);
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
     next();
 })
 
 
 // routes - react to request
-app.use('/api/workouts',workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
 
-// listen for request
-app.listen(process.env.PORT,()=>{
-    console.log('listening on port',process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+
+        // listen for request
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
